d26cfd341e0b04cde36ca3dd60588ad8b9ec0420
Andreina Chirinos <61394152+andregit37@users.noreply.github.com>
2022-07-04 09:30:43 -0400

    Fixed computation interest for negative number

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years); 
+    if (principal < 1)
+    {
+        alert("The principal value must be a positive number")
+        document.getElementById("principal").focus();
+    }
+    else
+    {
     document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark> ,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    }
 }
 
 function updateRate()
@@ -14,10 +22,12 @@ function updateRate()
     document.getElementById("rate_val").innerText=rateval+"%";
 }
 
-function positive(){
+function positive()
+{
     var principal = document.getElementById("principal").value;
        
-    if (principal < 1){
+    if (principal < 1)
+    {
         alert("The principal value must be a positive number")
         document.getElementById("principal").focus();
     }
