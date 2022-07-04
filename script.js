e1b71fe04685d6647f42c2c801a548762d5125e1
Andreina Chirinos <61394152+andregit37@users.noreply.github.com>
2022-07-04 10:31:59 -0400

    adding comments

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-function compute()
+function compute()                          //read all the input values and computes the simple interest  
 {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years); 
-    if (principal < 1)
+    var year = new Date().getFullYear()+parseInt(years); //sums the year value input with the actual year we're in
+    
+    
+    if (principal < 1)                      //this conditional emits an alert that only positive numbers are allowed to compute interest  
     {
         alert("The principal value must be a positive number")
-        document.getElementById("principal").focus();
+        document.getElementById("principal").focus();   
     }
-    else
+    else                                    //if theres a positive number on the principal input box then it shows compute interest button results
     {
     document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark> ,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
     }
 }
 
-function updateRate()
+function updateRate()                       //it assings on change the interest value on the range slider 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval+"%";
 }
 
-function positive()
-{
-    var principal = document.getElementById("principal").value;
-       
-    if (principal < 1)
-    {
-        alert("The principal value must be a positive number")
-        document.getElementById("principal").focus();
-    }
-}
+
